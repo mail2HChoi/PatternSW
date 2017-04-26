c73c66a9ea9e66563c2e26f21dd18fed61717c10
mail2HChoi <mail2HChoi@gmail.com>
2017-04-27 07:52:52 +0900

    Add files via upload

---
+++ 실습예제/0427/controller/DispatcherEx/DispatcherConEx.java
@@ -0,0 +1,56 @@
+package com.javalec.ex;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DispatcherConEx
+ */
+@WebServlet("/DispatcherConEx")
+public class DispatcherConEx extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DispatcherConEx() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet");
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost");
+		actionDo(request, response);
+	}
+	
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("actionDo");
+		
+		request.setAttribute("id", "abcde");
+		request.setAttribute("pw", "12345");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/dispacherJsp.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+}
